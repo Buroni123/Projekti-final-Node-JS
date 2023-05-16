29e327ee04124e2f33ae727fe664cffd17bf2cf9
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2023-05-16 16:28:02 +0200

    Create category.js

---
+++ models/category.js
@@ -0,0 +1,22 @@
+const Sequelize = require("sequelize");
+const sequelize = require("./../database");
+
+const Category = sequelize.define(
+  "category",
+  {
+    name: {
+      type: Sequelize.STRING(30),
+      allowNull: false,
+      unique: true,
+    },
+    status: {
+      type: Sequelize.ENUM("Aktiv", "Jo aktiv"),
+      defaultValue: "Aktiv",
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+module.exports = Category
