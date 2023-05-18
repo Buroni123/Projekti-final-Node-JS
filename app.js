fb7441e9f7f717069d6ec165ec7e0e4f53d01be3
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2023-05-18 19:03:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,54 @@ app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 
 app.post('/users', (req,res) => {
+    let name = req.body.name
+    let email = req.body.email;
+    let gender = req.body.gender;
+    let address = req.body.address;
+    let status = req.body.status;
     let password = bcrypt.hashSync(req.body.password, 6)
+    User.findOne({
+        where: {
+            name,
+            email,
+            address,
+        }
+    })
+    .then((user)=>{
+        if(user) {
+            res.status(400).json({
+                data: "User already exists"
+            })
+        }
+        else{
+            User.create({
+                name,
+                email,
+                gender,
+                address,
+                status,
+                password,
+        })
+        .then((newUser) => {
+        res.status(200).json({
+            status: 1,
+            data: newUser   ,
+            })
+          })
+        .catch((err) => {
+        res.status(400).json({
+            status:0,
+            data: err.message
+            })
+          })
+      }
+    })
+    .catch((err) => {
+        res.status(400).json({
+            status:0,
+            data: err.message
+      })
+    })
 })
 
 sequelize.sync()
