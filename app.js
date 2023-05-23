fcc072f938e9827b9ad8d6a678584cac69de5bc1
Buroni123 <83972181+Buroni123@users.noreply.github.com>
2023-05-23 18:48:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -64,5 +64,70 @@ app.post('/users', (req,res) => {
     })
 })
 
+app.delete("/users/:id", (req, res) => {
+  let userId = req.params.id;
+
+  User.destroy({
+    where: {
+      id: userId,
+    },
+  })
+    .then((deletedUserCount) => {
+      if (deletedUserCount === 0) {
+        res.status(404).json({
+          status: 0,
+          message: "User not found",
+        });
+      } else {
+        res.status(200).json({
+          status: 1,
+          message: "User deleted successfully",
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({
+        status: 0,
+        message: "An error occurred while deleting the user",
+        error: err.message,
+      });
+    });
+});
+
+app.put('/users/:id', (req,res) => {
+    let userId = req.params.id
+    let name = req.body.name;
+    let email = req.body.email;
+    let gender = req.body.gender;
+    let address = req.body.address;
+    let status = req.body.status;
+    let password = req.body.password
+    User.update({
+      name,
+      email,
+      gender,
+      address,
+      status,
+      password,
+    },
+    {
+        where: {
+            id: userId
+        }
+    })
+    .then((user)=>{
+        res.status(200).json({
+            status: 1,
+            message: "User updated successfully!"
+        })
+    })
+    .catch((err) => {
+        res.status(400).json({
+            status: 0,
+            data: err.message
+        })
+    })
+})
+
 sequelize.sync()
 app.listen(3000)
